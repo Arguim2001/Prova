7ebf952d0eb453e4cd467611f411b011d75f16e9
Arguim2001 <argu.aguiar@gmail.com>
2022-11-23 12:29:22 -0300

    Subindo SQL

---
+++ Prova Mentoria.sql
@@ -0,0 +1,12 @@
+create database prova_mentoria;
+use  prova_mentoria;
+create table usuario (
+id smallint primary key auto_increment,
+nome varchar(50) not null,
+CPF float NOT NULL,
+nascimento varcharacter (12) not null,
+endereço varchar (80)
+);
+
+select * from usuario ;
+
